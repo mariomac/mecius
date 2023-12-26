c2f54f0917f8bd33445fa6a7f88e08586545fab1
Mario Macias <mario.macias@grafana.com>
2023-12-26 17:43:05 +0100

    basic add/contains/rm operation and tests for sparse set

--- src/set.rs
+++ src/set.rs
@@ -1,26 +1,29 @@
 use std::marker::PhantomData;
 
+
 #[derive(Debug)]
 pub struct Sparse<T> {
-    dense: Vec<usize>,
+    dense: Vec<Entry<T>>,
     sparse: Vec<Option<usize>>,
-    phantom: PhantomData<T>,
 }
 
-// TODO: consider forcing elem to implement SliceIndex instead of Into<usize>
-impl<T: Into<usize>> Sparse<T> {
+#[derive(Debug)]
+struct Entry<T> {
+    key: usize,
+    value: T,
+}
+
+impl<T> Sparse<T> where Entry<T>: From<T> {
     pub fn new() -> Self {
         Self {
             dense: Vec::new(),
             sparse: Vec::new(),
-            phantom: PhantomData,
         }
     }
     pub fn with_capacity(cap: usize) -> Self {
         Self {
             dense: Vec::with_capacity(cap),
             sparse: Vec::with_capacity(cap),
-            phantom: PhantomData,
         }
     }
 
@@ -33,16 +36,16 @@ impl<T: Into<usize>> Sparse<T> {
     }
 
     pub fn insert(&mut self, elem: T) -> bool {
-        let idx: usize = elem.into();
-        if self.contains_idx(idx) {
+        let entry: Entry<T> = elem.into();
+        if self.contains_idx(entry.key) {
             // do nothing
             return true;
         }
-        self.dense.push(idx);
-        if idx >= self.sparse.len() {
-            self.sparse.resize_with(idx, || None);
+        if entry.key >= self.sparse.len() {
+            self.sparse.resize_with(entry.key, || None);
         }
-        self.sparse.insert(idx, Some(self.dense.len() - 1usize));
+        self.sparse.insert(entry.key, Some(self.dense.len()));
+        self.dense.push(entry);
         false
     }
 
@@ -54,31 +57,74 @@ impl<T: Into<usize>> Sparse<T> {
     }
 
     pub fn contains(&self, elem: T) -> bool {
-        self.contains_idx(elem.into())
+        let entry: Entry<T> = elem.into();
+        self.contains_idx(entry.key)
+    }
+
+    pub fn remove(&mut self, elem: T) -> bool {
+        let rm_entry: Entry<T> = elem.into();
+        let sparse_rm_entry = self.sparse.get(rm_entry.key);
+        if let Some(rm_entry_dense_idx) = *sparse_rm_entry.unwrap() {
+            // remove entry from sparse
+            self.sparse[rm_entry.key] = None;
+
+            // replace removed entry by last entry in dense
+            let last_entry = self.dense.pop().unwrap();
+            let last_entry_key = last_entry.key;
+            self.dense[rm_entry_dense_idx] = last_entry;
+            // repoint sparse entry for the moved element
+            self.sparse[last_entry_key] = Some(rm_entry.key);
+            true
+        } else {
+            false
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::set::Sparse;
+    use crate::set::{Entry, Sparse};
+
+    impl From<i32> for Entry<i32> {
+        fn from(value: i32) -> Self {
+            Entry {
+                key: value as usize,
+                value,
+            }
+        }
+    }
 
     #[test]
     fn push_contains() {
         let mut set = Sparse::new();
-        set.insert(1usize);
-        set.insert(3usize);
-        set.insert(133usize);
+        set.insert(1);
+        set.insert(3);
+        set.insert(133);
 
         assert!(set.contains(1));
         assert!(set.contains(3));
         assert!(set.contains(133));
-        let cnts = set.contains(2);
         assert!(!set.contains(2));
         assert!(!set.contains(4));
     }
 
     #[test]
-    fn remove() {}
+    fn remove() {
+        let mut set = Sparse::new();
+        set.insert(1);
+        set.insert(3);
+        set.insert(133);
+
+        assert!(set.remove(3));
+        assert!(!set.remove(4));
+        assert!(!set.remove(3));
+
+        assert!(set.contains(1));
+        assert!(set.contains(133));
+        assert!(!set.contains(2));
+        assert!(!set.contains(3));
+        assert!(!set.contains(4));
+    }
 
     #[test]
     fn contains() {}
